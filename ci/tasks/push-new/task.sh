5429386028903f3cf3b9b31ae0ea11a7fb9a0436
myminseok <myminseok@users.noreply.github.com>
2018-10-19 12:42:53 +0900

    Update task.sh

--- ci/tasks/push-new/task.sh
+++ ci/tasks/push-new/task.sh
@@ -11,8 +11,9 @@ rm *.deb
 cf version
 
 echo "cf login... $cf_api_url"
+set +x
 cf login -a $cf_api_url -u $cf_username -p $cf_password -o $cf_org -s $cf_space --skip-ssl-validation 2>&1 >/dev/null
-
+set -x
 cf_domain="apps.pas.pcf.net"
 
 cf apps
